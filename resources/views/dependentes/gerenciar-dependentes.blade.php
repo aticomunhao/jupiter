bbe7b5cf04aeb39b4abec8c6f3950dbd16a12c58
MathiasColetti <65311420+aticomunhao@users.noreply.github.com>
2023-06-22 20:53:50 -0300

    Evolução do gerenciar dependentes
    Evolução do gerenciar dependentes

--- resources/views/dependentes/gerenciar-dependentes.blade.php
+++ resources/views/dependentes/gerenciar-dependentes.blade.php
@@ -4,7 +4,11 @@
 @section('content')
     <div class="container">
         <div class="justify-content-center">
+            <div class="card">
+                <div class="card-body">
 
+                </div>
+            </div>
         </div>
     </div>
 
