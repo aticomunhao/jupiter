275b82def783e59891ac29efd31b4f5efd7f4d3a
MathiasColetti <88839208+MathiasColetti@users.noreply.github.com>
2023-07-01 19:48:43 -0300

    Alteração na view gerenciar-deoendentes

--- resources/views/dependentes/gerenciar-dependentes.blade.php
+++ resources/views/dependentes/gerenciar-dependentes.blade.php
@@ -2,13 +2,43 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="justify-content-center">
-            <div class="card">
-                <div class="card-body">
-                <h1>Olá Mundo</h1>
+            <div class="container">
+                <div class="justify-content-center">
+                    
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="row">
+                                <div class="col-2">Editar - Dependentes</div>
+                                <div class="col-8"> </div>
+                                <div class="col-2">aaa </div>
+                            </div>
+                        
+                        </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-1">a</div>
+                            <div class="col-3">
+                                <div class="card">
+                                    <div class="card-body bg-body-secondary">
+                                        Mathias Xavier Coletti aaaaa
+                                    </div>
+                                    </div>
+                                </div>
+                            <div class="col-5">a</div>
+                            <div class="col-3">
+                            <button type="button" class="btn btn-success btn-lg" style="padding: 5px 80px;">Novo &plus;</button>
+                            </div>
+                            
+                        </div>
+                        <div class="row">
+                            <div class="col-2"></div>
+                                <div class="col-8">
+
+                                </div>
+                            <div class="col-2"></div>
+                        </div>
+                    </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
 @endsection
