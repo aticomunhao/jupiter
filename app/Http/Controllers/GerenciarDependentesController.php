7a00b1b0c37e036cd4804ebf7e9f6cbf10a6d821
MathiasColetti <88839208+MathiasColetti@users.noreply.github.com>
2023-08-21 12:55:49 -0300

    Auto stash before merge of "main" and "origin/main"

--- app/Http/Controllers/GerenciarDependentesController.php
+++ app/Http/Controllers/GerenciarDependentesController.php
@@ -13,14 +13,16 @@ class GerenciarDependentesController extends Controller
      */
     public function index($idf)
     {
-        $funcionario = DB::select("select 
-                            f.id, 
-                            p.nome_completo 
-                            from funcionario f 
+        $funcionario = DB::select("select
+                            f.id,
+                            p.nome_completo
+                            from funcionario f
                             left join pessoa p on f.id_pessoa = p.id
                             where f.id = $idf");
         //dd($funcionario);
+<<<<<<< Updated upstream
 
+<<<<<<< Updated upstream
         $dependentes = DB::select("select 
                             d.id,
                             d.id_funcionario,
@@ -32,6 +34,15 @@ class GerenciarDependentesController extends Controller
                             from dependente d
                             left join tp_parentesco tpp on d.id_parentesco = tpp.id 
                             where d.id_funcionario = $idf");
+=======
+        $dependentes = DB::select("select
+                            id_funcionario
+                            from dependente d
+                            where = d.id_funcionario = $idf");
+
+        $dependentes = DB::select("select * from dependente where id_funcionario= $id");
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
         //dd($dependentes);
         return view('/dependentes/gerenciar-dependentes', compact('funcionario', 'dependentes'));
     }
@@ -58,6 +69,7 @@ class GerenciarDependentesController extends Controller
     {
         $funcionario = DB::select("select f.id, p.nome_completo from funcionario f left join pessoa p on f.id_pessoa = p.id where f.id = $id");
 
+<<<<<<< Updated upstream
         $vercpf = DB::table('dependente')
                     ->get('cpf');
 
@@ -65,28 +77,49 @@ class GerenciarDependentesController extends Controller
 
                     if ( $request->$cpf == $vercpf){
 
+=======
+        $cpf_dependente = DB::select('select cpf from dependente');
+>>>>>>> Stashed changes
 
-                        app('flasher')->addError('Existe outro cadastro usando este número de CPF');
-
-                        return redirect()->route('Batata', ['id' => $id]);
+        $cpf_depatual = $request->cpf_dep;
 
 
-                    }else{
-                        DB::table('dependente')->insert([
 
+        $igual = 0;
+
+<<<<<<< Updated upstream
                             'nome_dependente'=> $request->input('nomecomp_dep'),
                             'dt_nascimento'=> $request->input('dtnasc_dep'),
                             'cpf'=>$request->input('cpf_dep'),
                             'id_funcionario'=>$id,
                             'id_parentesco'=>$request->input('relacao_dep')
+=======
+        foreach ($cpf_dependente as $cpf_dependentes) {
+            if ($cpf_dependentes == $cpf_depatual) {
+                $igual = 1;
+            }
+        }
+>>>>>>> Stashed changes
 
-                        ]);
-                    }
+        if ($igual == 1) {
+            app('flasher')->addError('Existe outro cadastro usando este número de CPF');
+            return redirect()->route('Batata', ['id' => $id]);
+        }else {
+            DB::table('dependente')->insert([
 
+                'nome_dependente'=> $request
+                ->input('nomecomp_dep'),
+                'dt_nascimento'=> $request
+                ->input('dtnasc_dep'),
+                'cpf'=>$request
+                ->input('cpf_dep'),
+                'id_pessoa'=>$id,
+                'id_parentesco'=>$request->input('relacao_dep')
 
-        
-        app('flasher')->addSuccess('O cadastro do dependente foi realizado com sucesso.');
-        return redirect()->route('Batata', ['id' => $id]);
+            ]);
+            app('flasher')->addSuccess('O cadastro do dependente foi realizado com sucesso.');
+            return redirect()->route('Batata', ['id' => $id]);
+        }
     }
 
     /**
