56ba997621bea80469cb85079f450c205275dd9f
Humberto Nascimento <humberto.junior@comunhaoespirita.com>
2023-10-27 13:13:37 -0300

    view do Gerenciar setor Finalizada

--- app/Http/Controllers/GerenciarSetoresController.php
+++ app/Http/Controllers/GerenciarSetoresController.php
@@ -6,15 +6,47 @@ use Illuminate\Http\Request;
 use App\Models;
 use iluminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\CollectionorderBy;
 use Carbon\Carbon;
+
 
 class GerenciarSetoresController extends Controller
 {
-    public function index(){
-        $lista = DB:: select('select*from setores');
+    public function index(Request $request){
+
+    $lista = DB::table('setores AS s')
+    ->select('s.id AS ids', 's.nome', 's.usuario', 's.sigla', 's.dt_inicio', 's.dt_fim')
+    ->get();
 
       //dd($lista);
 
-       return view('/setores/gerenciar-setor');
+      $usuario = $request->usuario;
+
+      $nome = $request->nome;
+
+      $sigla = $request->sigla;
+
+      $dt_inicio = $request->dt_inicio;
+
+      $dt_fim = $request->dt_fim;
+
+    if ($request->usuario) {
+        $lista->where('setores.usuario', $request->usuario);
+    }
+
+    if ($request->nome) {
+        $lista->where('setores.nome', '=', $request->nome);
+    }
+
+    if ($request->sigla) {
+        $lista->where('setores.sigla', '=', $request->sigla);
+    }
+
+
+
+    $lista = $lista->sortBy([['sigla', 'asc'],['usuario', 'asc']]);
+
+
+       return view('/setores/gerenciar-setor', compact ('lista','usuario', 'nome', 'sigla', 'dt_inicio', 'dt_fim'));
     }
 }
--- resources/views/setores/gerenciar-setor.blade.php
+++ resources/views/setores/gerenciar-setor.blade.php
@@ -6,17 +6,18 @@
     <div class="col-12">
         <div class="row justify-content-center">
             <form action="{{route('gerenciar')}}" class="form-horizontal mt-4" method="GET">
-            <div class="row">
-                <div class="col-2">Funcionário
-                    <input class="form-control" maxlength="11" type="text" id="1" name="usuario">
+            <div class="row"  style="padding-left:5%">
+                <div class="col-2">Usuário
+                    <input class="form-control" maxlength="11" type="text" id="1" name="usuario" value="{{$usuario}}">
                 </div>
-                <div class="col-2">Nome
-                    <input class="form-control" maxlength="9" type="text" id="2" name="nome">
+                <div class="col-3">Setor
+                    <input class="form-control" maxlength="9" type="text" id="2" name="nome" value="{{$nome}}">
                 </div>
-                <div class="col-3">Sigla
-                    <input class="form-control" maxlength="50" type="text" id="3" name="siglaa">
+                <div class="col-1">Sigla
+                    <input class="form-control" maxlength="50" type="text" id="3" name="sigla" value="{{$lista[0]->sigla}}">
                 </div>
-                <div class="col"><br>
+
+                <div class="col" style="padding-left:20%"><br>
                     <input class="btn btn-light btn-sm" style="font-size: 0.9rem; box-shadow: 1px 2px 5px #000000; margin:5px;" type="submit" value="Pesquisar">
 
                     <a href=""><button class="btn btn-primary" type="button" value="" style="font-size: 0.9rem; box-shadow: 1px 2px 5px #000000; margin:5px;" >Limpar</button></a>
@@ -31,21 +32,41 @@
     <div class="table">
         <table class="table table-sm table-striped table-bordered border-secondary table-hover align-middle">
             <thead style="text-align: center;">
-                <tr style="background-color: #d6e3ff; font-size:17px; color:#000000">
-                    <th class="col-1">Setor</th>
+                <tr style="background-color: #365699; font-size:19px; color:#000000">
+                    <th class="col-3">Setor</th>
                     <th class="col-1">Sigla</th>
-                    <th class="col-4">Dt_inicio</th>
+                    <th class="col-1">Dt_inicio</th>
                     <th class="col-1">Dt_final</th>
-                    <th class="col-4">Ações</th>
+                    <th class="col-4">Usuário</th>
+                    <th class="col-2">Ações</th>
+
                 </tr>
             </thead>
-            <tbody style="font-size: 14px; color:#000000;">
+            <tbody style="font-size: 16px; color:#000000;">
                 <tr>
                     @foreach ($lista as $listas)
-                        <td scope="">{{$listas->setor}}</td>
-                        <td scope="">{{$listas->sigla}}</td>
-                        <td scope="">{{$listas->dt_incio}}</td>
-                        <td scope="">{{$listas->dt_fim}}</td>
+                        <td scope="">{{$listas->nome}}</td>
+                        <td scope=""><center>{{$listas->sigla}}</center></td>
+                        <td scope=""><center>{{$listas->dt_inicio}}</center></td>
+                        <td scope=""><center>{{$listas->dt_fim}}</center></td>
                         <td scope="">{{$listas->usuario}}</td>
-                    @endforeach
+
+
+                        <td scope="">
+
+                            <a href=""><button type="button" class="btn btn-outline-warning btn-sm"><i class="bi-pencil" style="font-size: 1rem; color:#303030;"></i></button></a>
+                            <a href=""><button type="button" class="btn btn-outline-primary btn-sm"><i class="bi-search" style="font-size: 1rem; color:#303030;"></i></button></a>
+                            <button type="button" class="btn btn-outline-primary btn-sm"><i class="bi-people-fill" style="font-size: 1rem;color:#303030; "></i></button></a>
+                            <button type="button" class="btn btn-outline-danger btn-sm" data-bs-toggle="modal" class="btn btn-outline-danger btn-sm"><i class="bi-trash" style="font-size: 1rem; color:#303030;"></i></button>
+
+
+                            </td>
+
+
+
+                    </td>
+            </tr>
+
+                </tr>
+                @endforeach
     @endsection
