671010edf7ed3a0324c78a0a01207876296e9bd4
MathiasColetti <mathiasc2004@gmail.com>
2023-12-05 14:16:48 -0300

    View Gerenciar Cargo Regular

--- resources/views/cargosregulares/gerenciar-cargo-regular.blade.php
+++ resources/views/cargosregulares/gerenciar-cargo-regular.blade.php
@@ -1,8 +1,49 @@
 @extends('layouts.app')
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-12">
-            
+
+@section('content')
+    <div class="container-fluid">
+        <br>
+        <div class="row">
+            <div class="container-fluid">
+                <fieldset class="border border-primary rounded">
+                    <div class="card">
+
+                        <h5 class="card-header">
+
+                            <div class="row">
+                                <div class="col-lg-4 col-sm-12">
+
+                                    <span>
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <span>Gerenciar Cargos Regulares</span>
+                                            </div>
+                                        </div>
+                                    </span>
+                                </div>
+                                <div class="col-lg-5">
+                                    <br>
+                                </div>
+                                <div class="col-lg-3 col-md-12">
+                                    <button type="button" class="btn btn-success"
+                                            style="padding: 15px 30%; box-shadow: 4px 3px 2px rgba(0, 0, 0, 0.836);margin-top: 1%;width:100%">
+                                        Novo &plus
+                                    </button>
+
+
+                                </div>
+                            </div>
+                        </h5>
+
+                        <div class="card-body">
+
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.
+                            </p>a
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+            </div>
+            </fieldset>
         </div>
     </div>
-</div>
+@endsection
