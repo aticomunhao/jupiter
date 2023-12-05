5de6d92c9f08b6c92e5c74d0d4c2bda782a36a8e
MathiasColetti <mathiasc2004@gmail.com>
2023-12-05 17:00:57 -0300

    View

--- resources/views/cargosregulares/gerenciar-cargo-regular.blade.php
+++ resources/views/cargosregulares/gerenciar-cargo-regular.blade.php
@@ -12,7 +12,6 @@
 
                             <div class="row">
                                 <div class="col-lg-4 col-sm-12">
-
                                     <span>
                                         <div class="card">
                                             <div class="card-body">
@@ -26,20 +25,37 @@
                                 </div>
                                 <div class="col-lg-3 col-md-12">
                                     <button type="button" class="btn btn-success"
-                                            style="padding: 15px 30%; box-shadow: 4px 3px 2px rgba(0, 0, 0, 0.836);margin-top: 1%;width:100%">
-                                        Novo &plus
+                                            style="padding: 10px 30%; box-shadow: 4px 3px 2px rgba(0, 0, 0, 0.836);margin-top: 1%;width:100%">
+                                        Novo &plus;
                                     </button>
-
-
                                 </div>
                             </div>
                         </h5>
 
                         <div class="card-body">
 
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.
-                            </p>a
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <table
+                                class="table table table-striped table-bordered border-secondary table-hover align-middle justify-content-center">
+                                <thead class="text-align: center;">
+                                <tr style="background-color: #d6e3ff; font-size:17px; color:#000000">
+                                    <th scope="col">#</th>
+                                    <th scope="col">First</th>
+                                    <th scope="col">Last</th>
+                                    <th scope="col">Handle</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($cargosregulares as $cargoregular)
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                    </tr>
+                                @endforeach
+
+                                </tbody>
+                            </table>
                         </div>
                     </div>
             </div>
