31da1578e7cd20474c784f3ca2aa582cf8aaf0cf
MathiasColetti <mathiasc2004@gmail.com>
2024-01-03 11:15:21 -0300

    Choro do Beto

--- app/Http/Controllers/GerenciarHierarquiaController.php
+++ app/Http/Controllers/GerenciarHierarquiaController.php
@@ -11,71 +11,79 @@ use Carbon\Carbon;
 
 
 class GerenciarHierarquiaController extends Controller
-{ 
+{
     public function index(Request $request)
     {
 
-      $nivel = DB::table('tp_nivel_setor')->select('tp_nivel_setor.id AS id_nivel', 'nome as nome_nivel')->get();
+        $nivel = DB::table('tp_nivel_setor')->select('tp_nivel_setor.id AS id_nivel', 'nome as nome_nivel')->get();
 
-      $setor = DB::table('setor')
-      ->leftJoin('setor AS substituto', 'setor.substituto', '=', 'substituto.id')
-      ->select('setor.id AS id_setor', 'setor.nome AS nome_setor', 'setor.sigla', 'setor.dt_inicio', 'setor.status', 'substituto.sigla AS nome_substituto')
-      ->get();
-  
-      //dd($setor);
-      $id_nivel = $request->id_nivel;
-      $id_setor = $request->id_setor;
-      $sigla = $request->input('sigla');
-      $status = $request->input('status');
-      $dt_inicio = $request->input('dt_inicio');
-      $nome_substituto = $request->nome_substituto;
+        $setor = DB::table('setor')
+            ->leftJoin('setor AS substituto', 'setor.substituto', '=', 'substituto.id')
+            ->select('setor.id AS id_setor', 'setor.nome AS nome_setor', 'setor.sigla', 'setor.dt_inicio', 'setor.status', 'substituto.sigla AS nome_substituto')
+            ->get();
 
-      $nome_nivel = $request->input('nome_nivel');
-      $nome_setor = $request->input('nome_setor');
+        //dd($setor);
+        $id_nivel = $request->id_nivel;
+        $id_setor = $request->id_setor;
+        $sigla = $request->input('sigla');
+        $status = $request->input('status');
+        $dt_inicio = $request->input('dt_inicio');
+        $nome_substituto = $request->nome_substituto;
 
-      if ($nome_nivel) {
-         $nivel->where('nome', $nome_nivel);
-     }
+        $nome_nivel = $request->input('nome_nivel');
+        $nome_setor = $request->input('nome_setor');
 
-     if ($nome_setor) {
-         $setor->where('nome', $nome_setor);
-     }
+        if ($nome_nivel) {
+            $nivel->where('nome', $nome_nivel);
+        }
 
-     if ($id_setor) {
-      $setor->where('nome', $id_setor);
-     }
+        if ($nome_setor) {
+            $setor->where('nome', $nome_setor);
+        }
 
-     if ($id_nivel) {
-      $nivel->where('nome', $id_nivel);
-     }      
-     
-     if ($sigla) {
-      $sigla->where('nome', $sigla);
-     }     
+        if ($id_setor) {
+            $setor->where('nome', $id_setor);
+        }
 
-    return view('/setores/Gerenciar-hierarquia', compact('nome_nivel','nivel','setor', 'nome_setor', 'id_nivel', 'id_setor', 'sigla', 'dt_inicio', 'status', 'nome_substituto'));
+        if ($id_nivel) {
+            $nivel->where('nome', $id_nivel);
+        }
+
+        if ($sigla) {
+            $sigla->where('nome', $sigla);
+        }
+
+        return view('/setores/Gerenciar-hierarquia', compact('nome_nivel', 'nivel', 'setor', 'nome_setor', 'id_nivel', 'id_setor', 'sigla', 'dt_inicio', 'status', 'nome_substituto'));
     }
-   public function create(Request $request){
 
-      $nome_nivel = $request->input('nome_nivel');
-      $nome_setor = $request->input('nome_setor');
+    public function create(Request $request)
+    {
 
-      $niveis = DB::table('tp_nivel_setor')->select('id AS id_nivel', 'nome AS nome_nivel');
-      $setor = DB::table('setor')->select('id AS id_setor', 'nome AS nome_setor');
+        $nome_nivel = $request->input('nome_nivel');
+        $nome_setor = $request->input('nome_setor');
+
+        $niveis = DB::table('tp_nivel_setor')->select('id AS id_nivel', 'nome AS nome_nivel');
+        $setor = DB::table('setor')->select('id AS id_setor', 'nome AS nome_setor');
 
 
-      if ($nome_nivel) {
-          $niveis->where('nome', $nome_nivel);
-      }
+        if ($nome_nivel) {
+            $niveis->where('nome', $nome_nivel);
+        }
 
-      if ($nome_setor) {
-          $setor->where('nome', $nome_setor);
-      }
+        if ($nome_setor) {
+            $setor->where('nome', $nome_setor);
+        }
 
-      $niveis = $niveis->get();
-      $setores = $setor->get();
+        $niveis = $niveis->get();
+        $setores = $setor->get();
 
-      return view('/setores/Gerenciar-hierarquia', compact('nome_nivel', 'nome_setor', 'niveis', 'setor'));
-  }
+        return view('/setores/Gerenciar-hierarquia', compact('nome_nivel', 'nome_setor', 'niveis', 'setor'));
+    }
+
+    public function retornaSetor($idsetor) {
+        $setoresfilhos = DB::table('setor')->where('setor_pai', $idsetor)->get();
+
+        return response()->json(['setoresfilhos' => $setor]);
+    }
 }
 
--- resources/views/setores/Gerenciar-hierarquia.blade.php
+++ resources/views/setores/Gerenciar-hierarquia.blade.php
@@ -1,44 +1,44 @@
 @extends('layouts.app')
 
-
 @section('content')
-<br>
+    <br>
 
-<div class="container">
+    <div class="container">
 
-    <div class="border border-primary" style=" border-radius: 5px;">
-        <div class="card">
-            <div class="card-header">
-                <div class="row" style="margin-left:5px" action="{{route('gerenciar')}}" method="GET">
-                    <div class="col-2">
-                        <label for="1">Nivel</label>
-                        <select id="1" class="form-select" name="nivel" value="{{$nome_nivel}}" type="text">
-                            <option></option>
-                            @foreach($nivel as $niveis)
-                            <option value="{{$niveis->id_nivel}}">{{$niveis->nome_nivel}}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="col-3">
-                        <label for="1">Setor</label>
-                        <select id="2" class="form-select" name="setor" type="text" value="{{$nome_setor}}">
-                            <option></option>
-                            @foreach($setor as $setores)
-                            <option value="{{$setores->id_setor}}">{{$setores->nome_setor}}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="col" style="padding-top:25px">
-                        <a href="" type="button" value="" class="btn btn-danger">Cancelar</a>
-                        <a href="" type="button" class="btn btn-primary" value="">Limpar</a>
-                        <input type="submit" value="Confirmar" class="btn btn-success">
+        <div class="border border-primary" style="border-radius: 5px;">
+            <div class="card">
+                <div class="card-header">
+                    <div class="row" style="margin-left:5px" action="{{ route('gerenciar') }}" method="GET">
+                        <div class="col-2">
+                            <label for="1">Nivel</label>
+                            <select id="idnivel" class="form-select" name="nivel" value="{{ $nome_nivel }}"
+                                type="text">
+                                <option></option>
+                                @foreach ($nivel as $niveis)
+                                    <option value="{{ $niveis->id_nivel }}">{{ $niveis->nome_nivel }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="col-3">
+                            <label for="1">Setor</label>
+                            <select id="idsetor" class="form-select" name="setor" type="text"
+                                value="{{ $nome_setor }}" disabled>
+                                <option></option>
+                                @foreach ($setor as $setores)
+                                    <option value="{{ $setores->id_setor }}">{{ $setores->nome_setor }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="col" style="padding-top:25px">
+                            <a href="" type="button" value="" class="btn btn-danger">Cancelar</a>
+                            <a href="" type="button" class="btn btn-primary" value="">Limpar</a>
+                            <input type="submit" value="Confirmar" class="btn btn-success">
+                        </div>
                     </div>
                 </div>
                 <div class="card-body">
-
                     <hr>
                     <h5 style="padding-top:20px" class="card-title">Hierarquia de Setores</h5>
-
                     <div class="row" style="padding-top:20px">
                         <div class="col-2">
                             <label for="3">Ordenar</label>
@@ -52,20 +52,21 @@
                         </div>
                         <div class="col-2">
                             <label for="5">Nivel</label>
-                            <select id="1" class="form-select" name="nivel" value="{{$nome_nivel}}" type="text">
+                            <select id="1" class="form-select" name="nivel" value="{{ $nome_nivel }}"
+                                type="text">
                                 <option></option>
-                                @foreach($nivel as $niveis)
-                                <option value="{{$niveis->id_nivel}}">{{$niveis->nome_nivel}}</option>
+                                @foreach ($nivel as $niveis)
+                                    <option value="{{ $niveis->id_nivel }}">{{ $niveis->nome_nivel }}</option>
                                 @endforeach
                             </select>
                         </div>
                         <div class="col" style="padding-top:25px">
                             <a href="" type="button" class="btn btn-success" value="">Confirmar</a>
                         </div>
-
                     </div>
                     <div class="table" style="padding-top:20px">
-                        <table class="table table-sm table-striped table-bordered border-secondary table-hover align-middle">
+                        <table
+                            class="table table-sm table-striped table-bordered border-secondary table-hover align-middle">
                             <thead style="text-align: center;">
                                 <tr style="background-color: #365699; font-size:19px; color:#ffffff">
                                     <th class="col-1"><input type="checkbox" id="masterCheckBox"></th>
@@ -78,26 +79,26 @@
                             </thead>
                             <tbody style="font-size: 15px; color:#000000;">
                                 @foreach ($setor as $setores)
-                                <tr>
-                                    <td scope="">
-                                        <center><input type="checkbox" class="checkBox"></center>
-                                    </td>
-                                    <td scope="">
-                                        <center>{{$setores->nome_setor}}</center>
-                                    </td>
-                                    <td scope="">
-                                        <center>{{$setores->sigla}}</center>
-                                    </td>
-                                    <td scope="">
-                                        <center>{{$setores->dt_inicio}}</center>
-                                    </td>
-                                    <td scope="">
-                                        <center>{{$setores->status?'Ativo':'Inativo'}}</center>
-                                    </td>
-                                    <td scope="">
-                                        <center>{{$setores->nome_substituto}}</center>
-                                    </td>
-                                </tr>
+                                    <tr>
+                                        <td scope="">
+                                            <center><input type="checkbox" class="checkBox"></center>
+                                        </td>
+                                        <td scope="">
+                                            <center>{{ $setores->nome_setor }}</center>
+                                        </td>
+                                        <td scope="">
+                                            <center>{{ $setores->sigla }}</center>
+                                        </td>
+                                        <td scope="">
+                                            <center>{{ $setores->dt_inicio }}</center>
+                                        </td>
+                                        <td scope="">
+                                            <center>{{ $setores->status ? 'Ativo' : 'Inativo' }}</center>
+                                        </td>
+                                        <td scope="">
+                                            <center>{{ $setores->nome_substituto }}</center>
+                                        </td>
+                                    </tr>
                                 @endforeach
                             </tbody>
                         </table>
@@ -108,29 +109,88 @@
                     </div>
 
                     <script>
-                        const masterCheckBox = document.getElementById('masterCheckBox');
-                        const checkBoxes = Array.from(document.querySelectorAll('.checkBox'));
+                        document.addEventListener('DOMContentLoaded', function() {
+                                    const masterCheckBox = document.getElementById('masterCheckBox');
+                                    const checkBoxes = Array.from(document.querySelectorAll('.checkBox'));
 
-                        // Marca ou desmarca todas as checkboxes de acordo com a checkbox master
-                        masterCheckBox.onchange = (event) => checkBoxes.forEach(e => {
-                            e.checked = event.target.checked;
-                            changeBackground(e);
-                        });
+                                    // Marca ou desmarca todas as checkboxes de acordo com a checkbox master
+                                    masterCheckBox.addEventListener('change', function(event) {
+                                        checkBoxes.forEach(function(e) {
+                                            e.checked = event.target.checked;
+                                            changeBackground(e);
+                                        });
+                                    });
 
-                        // Marca masterCheckBox se todas as outras estiverem marcadas
-                        // Desmarca se pelo menos uma estiver desmarcada
-                        checkBoxes.forEach(e => e.onchange = (event) => {
-                            masterCheckBox.checked = checkBoxes.every(f => f.checked);
-                            changeBackground(e);
-                        });
+                                    // Marca masterCheckBox se todas as outras estiverem marcadas
+                                    // Desmarca se pelo menos uma estiver desmarcada
+                                    checkBoxes.forEach(function(e) {
+                                        e.addEventListener('change', function(event) {
+                                            masterCheckBox.checked = checkBoxes.every(function(f) {
+                                                return f.checked;
+                                            });
+                                            changeBackground(e);
+                                        });
+                                    });
 
-                        // Destaca background das linhas selecionadas
-                        function changeBackground(input) {
-                            const tableRow = input.parentElement.parentElement;
-                            if (input.checked) tableRow.style.background = '#aaa';
-                            else tableRow.style.background = '';
-                        }
+                                    // Destaca background das linhas selecionadas
+                                    function changeBackground(input) {
+                                        const tableRow = input.parentElement.parentElement;
+                                        if (input.checked) tableRow.style.background = '#aaa';
+                                        else tableRow.style.background = '';
+                                    }
+
+                                    //JQUERY
+                                    $(document).ready(function() {
+                                        const idNivelSelect = $('#idnivel');
+                                        const idSetorSelect = $('#idsetor');
+                                        const nomeSetorElement = $('#nomeSetor');
+                                        const responsavelSetorElement = $('#responsavelSetor');
+
+                                        idNivelSelect.on('change', function() {
+                                            const nivel = $(this).val();
+                                            idSetorSelect.prop('disabled', !nivel);
+
+                                            if (nivel) {
+                                                $.ajax({
+                                                    type: "get",
+                                                    url: "/retornar-setor/" + nivel,
+                                                    dataType: "json",
+                                                    success: function(response) {
+                                                        if (response && response.setor) {
+                                                            // Update the displayed information
+                                                            nomeSetorElement.text('Nome: ' + response.setor
+                                                                .nome_setor);
+                                                            responsavelSetorElement.text('Responsável: ' +
+                                                                response.setor.responsavel);
+
+                                                            // Update options in the idsetor select element
+                                                            idSetorSelect.empty(); // Clear existing options
+                                                            idSetorSelect.append(
+                                                            '<option></option>'); // Add an empty option
+
+                                                            // Add options based on the data returned from the server
+                                                            $.each(response.setores, function(index, setor) {
+                                                                idSetorSelect.append('<option value="' +
+                                                                    setor.id_setor + '">' + setor
+                                                                    .nome_setor + '</option>');
+                                                            });
+
+                                                            // Enable the idsetor select element
+                                                            idSetorSelect.prop('disabled', false);
+                                                        } else {
+                                                            console.log('Setor não encontrado.');
+                                                        }
+                                                    },
+                                                    error: function(error) {
+                                                        console.log(error);
+                                                    }
+                                                });
+                                            }
+                                        });
+                                    });
                     </script>
-
-
-                    @endsection
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
