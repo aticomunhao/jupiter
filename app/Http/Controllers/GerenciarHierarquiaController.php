2b4d81b29b9d5119c1fb7749b30470552905dadf
Humberto <humberto.junior@comunhaoespirita.com>
2024-01-04 13:16:50 -0300

    tentando terminar o select dinamico hierarquia

--- app/Http/Controllers/GerenciarHierarquiaController.php
+++ app/Http/Controllers/GerenciarHierarquiaController.php
@@ -15,93 +15,65 @@ class GerenciarHierarquiaController extends Controller
     public function index(Request $request)
     {
 
-        $nivel = DB::table('tp_nivel_setor')->select('tp_nivel_setor.id AS id_nivel', 'nome as nome_nivel')->get();
+        $nivel = DB::table('tp_nivel_setor')->select('tp_nivel_setor.id AS id_nivel', 'tp_nivel_setor.nome as nome_nivel');
 
         $setor = DB::table('setor')
             ->leftJoin('setor AS substituto', 'setor.substituto', '=', 'substituto.id')
-            ->select('setor.id AS id_setor', 'setor.nome AS nome_setor', 'setor.sigla', 'setor.dt_inicio', 'setor.status', 'substituto.sigla AS nome_substituto')
-            ->get();
-            
+            ->select('setor.id AS id_setor', 'setor.nome AS nome_setor', 'setor.sigla', 'setor.dt_inicio', 'setor.status', 'substituto.sigla AS nome_substituto');
+
 
         //dd($setor);
         $id_nivel = $request->id_nivel;
         $id_setor = $request->id_setor;
-        $sigla = $request->input('sigla');
-        $status = $request->input('status');
-        $dt_inicio = $request->input('dt_inicio');
+        $sigla = $request->sigla;
+        $status = $request->status;
+        $dt_inicio = $request->dt_inicio;
         $nome_substituto = $request->nome_substituto;
+        $nome_nivel = $request->nome_nivel;
+        $nome_setor = $request->nome_setor;
 
-        $nome_nivel = $request->input('nome_nivel');
-        $nome_setor = $request->input('nome_setor');
-
-        if ($nome_nivel) {
-            $nivel->where('nome', $nome_nivel);
+        if ($request->id_setor) {
+            $setor->where('id_setor', $request->id_setor);
         }
 
-        if ($nome_setor) {
-            $setor->where('nome', $nome_setor);
+        if ($request->id_nivel) {
+            $nivel->where('id_nivel', $request->id_nivel);
         }
 
-        if ($id_setor) {
-            $setor->where('nome', $id_setor);
+        if ($request->status) {
+            $status->where('status', $request->status);
         }
 
-        if ($id_nivel) {
-            $nivel->where('nome', $id_nivel);
+        if ($request->sigla) {
+            $sigla->where('sigla', $request->sigla);
         }
 
-        if ($sigla) {
-            $sigla->where('nome', $sigla);
+        if ($request->nome_nivel) {
+            $nivel->where('nome_nivel',  'LIKE', '%' . $request->nome_nivel . '%');
         }
+
+        if ($request->nome_setor) {
+            $setor->where('nome_setor',  'LIKE', '%' . $request->nome_nivel . '%');
+        }
+
+        $nivel = $nivel->orderBy('id_nivel', 'asc')->orderBy('nome_nivel', 'asc')->paginate(10);
+
+        $setor = $setor->orderBy('status', 'asc')->orderBy('nome_setor', 'asc')->paginate(10);
+
 
         return view('/setores/Gerenciar-hierarquia', compact('nome_nivel', 'nivel', 'setor', 'nome_setor', 'id_nivel', 'id_setor', 'sigla', 'dt_inicio', 'status', 'nome_substituto'));
     }
 
-    public function create(Request $request)
-    {
 
-        $nome_nivel = $request->input('nome_nivel');
-        $nome_setor = $request->input('nome_setor');
-
-        $niveis = DB::table('tp_nivel_setor')->select('id AS id_nivel', 'nome AS nome_nivel');
-        $setor = DB::table('setor')->select('id AS id_setor', 'nome AS nome_setor');
-
-
-        if ($nome_nivel) {
-            $niveis->where('nome', $nome_nivel);
-        }
-
-        if ($nome_setor) {
-            $setor->where('nome', $nome_setor);
-        }
-
-        $niveis = $niveis->get();
-        $setores = $setor->get();
-
-        return view('/setores/Gerenciar-hierarquia', compact('nome_nivel', 'nome_setor', 'niveis', 'setor'));
-    }
-
-    public function retornaSetor($idsetor)
-    {
-        $setoresfilhos = DB::table('setor')->where('id_nivel', $idsetor)->get();
-
-        dd($setoresfilhos);
-    
-        return response()->json(['setoresfilhos' => $setoresfilhos]);
-    }
-    
 
     public function obterSetoresPorNivel($id_nivel)
     {
         $set = DB::table('setor as s')->where('id_nivel', $id_nivel)->select('s.nome')->get();
-    
+
         if ($set->isNotEmpty()) {
-            return response()->json($set); 
+            return response()->json($set);
         }
-    
+
         return response()->json(['message' => 'Nenhum setor encontrado para o ID de nível fornecido']);
     }
-    
-    
-
 }
