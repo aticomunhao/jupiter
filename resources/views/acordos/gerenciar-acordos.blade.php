0e017b4eaf0104a82c11cb8bf6efeff0fc8df413
nathanFAO <nathan1fellipe@gmail.com>
2024-02-09 14:43:28 -0300

    Teste

--- resources/views/acordos/gerenciar-acordos.blade.php
+++ resources/views/acordos/gerenciar-acordos.blade.php
@@ -1,134 +1,122 @@
 @extends('layouts.app')
-<title>Gerenciar Acordos </title>
+@section('head')
+    <title>Gerenciar Acordos</title>
+@endsection
 @section('content')
-    <div class="container">
-        <div class="justify-content-center">
-            <div class="col-12">
-                <br>
-                <div class="card">
-                    <fieldset class="border rounded border-primary">
-                        <div class="card-header">
-                            <div class="row">
-                                <div class="col-6"><span
-                                        style=" color: rgb(26, 53, 173); font-size:15px;">Gerenciar-Acordos</span>
-                                </div>
-                                <div class="col-6">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col">
-                                    <div class="card">
-                                        <div class="card-body bg-body-secondary">
-                                            <div style="color: rgb(26, 53, 173); font-size:15px;">
-                                                {{ $funcionario->nome_completo }}</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-4"></div>
-                                <div class="col-1">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <br />
+    <div class="container"> {{-- Container completo da página  --}}
+        <div class="card" style="border-color: #5C7CB6;">
+            <div class="card-header">
+                Gerenciar Acordos
+            </div>
+            <div class="card-body">
+                <div class="row"> {{-- Linha com o nome e botão novo --}}
+                    <div class="col-md-6 col-12">
+                        <input class="form-control" type="text" value="{{ $funcionario->nome_completo }}" id="iddt_inicio"
+                            name="dt_inicio" required="required" disabled>
 
-                                    <div class="row">
-                                        <span style="margin-top: 15px; margin-left: -18px"><a
-                                                href="/incluir-acordos/{{ $funcionario->id }}"><button type="button"
-                                                    class="btn btn-success btn-sm"
-                                                    style="padding: 5px 80px;margin-right:100px;font-size: 0.9rem; box-shadow: 1px 2px 5px #000000; margin:5px;">Novo&plus;</button></a></span>
-                                    </div>
-                                </div>
-                                <div class="col-2"></div>
-                            </div>
-                            <hr>
-                            <div class="table">
-                                <table
-                                    class="table table-sm table-striped table-bordered border-secondary table-hover align-middle">
-                                    <thead style="text-align: center;">
-                                        <tr style="background-color: #d6e3ff; font-size:17px; color:#000000">
-                                            <th class="col-2">Tipo de Acordo</th>
-                                            <th class="col-2">Data de Inicio</th>
-                                            <th class="col-1">Valido</th>
-                                            <th class="col-2">Data de Fim</th>
-                                            <th class="col-3">Observações</th>
-                                            <th class="col-1">Arquivo</th>
-                                            <th class="col-1">Ações</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody style="font-size: 14px; color:#000000;">
-                                        <tr>
-                                            @foreach ($acordos as $acordo)
-                                                <td scope="">{{ $acordo->nome }}</td>
-                                                <td scope="">
-                                                    {{ \Carbon\Carbon::parse($acordo->data_inicio)->format('d/m/Y') }}</td>
+                    </div>
+                    <div class="col-md-3 offset-md-3 col-12 mt-4 mt-md-0"> {{-- Botão de incluir --}}
+                        <a href="/incluir-acordos/{{ $funcionario->id }}" class="col-6"><button type="button"
+                                class="btn btn-success col-md-8 col-12">Novo+</button></a>
+                    </div>
+                </div>
+                <br />
+                <hr />
 
-                                                <td scope="">{{ $acordo->valido }}</td>
-                                                <td scope="">
-                                                    {{ \Carbon\Carbon::parse($acordo->data_fim)->format('d/m/Y') }}</td>
-                                                <td scope="">{{ $acordo->observacao }}</td>
-                                                <td scope="">
-                                                    <a href="{{ asset("$acordo->caminho") }}"
-                                                        class="btn btn-sm btn-outline-secondary">
-                                                        <i class="bi bi-archive"></i>
-                                                    </a>
+
+                <div class="table-responsive"> {{-- Faz com que a tabela não grude nas bordas --}}
+                    <div class="table">
+                        <table class="table table-striped table-bordered border-secondary table-hover align-middle">
+                            <thead style="text-align: center;"> {{-- Text-align gera responsividade abaixo de Large --}}
+                                <tr class="align-middle" style="background-color: #d6e3ff; font-size:17px; color:#000000">
+                                    <th class="col-2">Tipo de Acordo</th>
+                                    <th class="col">Data de Inicio</th>
+                                    <th class="col">Valido</th>
+                                    <th class="col">Data de Fim</th>
+                                    <th class="col">Observações</th>
+                                    <th class="col">Arquivo</th>
+                                    <th class="col">Ações</th>
+
+                            </thead>
+                            <tbody style="font-size: 15px; color:#000000;">
+                                @foreach ($acordos as $acordo)
+                                    <tr>
+                                        <td class="text-center">{{ $acordo->nome }}</td>{{-- tipo de acordo --}}
+                                        <td class="text-center">
+                                            {{ \Carbon\Carbon::parse($acordo->data_inicio)->format('d/m/Y') }}</td>
+                                        {{-- data de inicio --}}
+                                        <td>{{ $acordo->valido }}</td>
+                                        <td>{{ \Carbon\Carbon::parse($acordo->data_fim)->format('d/m/Y') }}</td>
+
+                                        <td class="text-center">{{ $acordo->observacao }}</td>
+                                        <td class="text-center"><a href="{{ asset("$acordo->caminho") }}"
+                                                class="btn  btn-outline-secondary" data-tt="tooltip" data-placement="top"
+                                                title="Visualizar">
+                                                <i class="bi bi-archive"></i>
+                                            </a></td>
+                                        {{--  Área de ações  --}}
+                                        <center>
+                                            <td>
+                                                <a href="/editar-acordo/{{ $acordo->id }}" class="btn btn-outline-warning"
+                                                    data-tt="tooltip" data-placement="top"
+                                                    title="Editar">{{--  Botão editar  --}}
+                                                    <i class="bi bi-pencil"></i></a>
+                                                <button class="btn btn-outline-danger" {{-- Botão que aciona o modal  --}}
+                                                    data-bs-toggle="modal" data-bs-target="#A{{ $acordo->id }}"
+                                                    data-tt="tooltip" data-placement="top" title="Excluir">
+                                                    <i class="bi bi-trash"></i></button>
                                                 </td>
-
-
-
-                                                <td scope="">
-                                                    <!-- Button trigger modal -->
-                                                    <button type="button" class="btn btn-outline-danger delete-btn btn-sm"
-                                                        data-bs-toggle="modal" data-bs-target="#A{{ $acordo->id }}"><i
-                                                            class="bi bi-trash"></i></button>
-
-
-                                                    <!-- Modal -->
-
-                                                    <div class="modal fade" id="A{{ $acordo->id }}" tabindex="-1"
-                                                        aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                        <div class="modal-dialog">
-                                                            <div class="modal-content">
-                                                                <div class="modal-header">
-                                                                    <div class="row">
-                                                                        <h2>Excluir Acordo</h2>
-                                                                    </div>
-                                                                    <button type="button" class="btn-close"
-                                                                        data-bs-dismiss="modal" aria-label="Close"></button>
+                                                <div class="modal fade"{{--  Modal  --}} id="A{{ $acordo->id }}"
+                                                    tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                    <div class="modal-dialog">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header"
+                                                                style="background-color:rgba(202, 61, 61, 0.911);">
+                                                                <div class="row">
+                                                                    <h2 style="color:white;">Excluir Dependente</h2>
                                                                 </div>
-                                                                <div class="modal-body">
-                                                                    <p class="fw-bold alert alert-danger text-center">Você
-                                                                        realmente deseja
-                                                                        <br>
-                                                                        <span class="fw-bolder fs-5">EXCLUIR
-                                                                            {{ $acordo->nome }}
-                                                                        </span>
-                                                                    </p>
-                                                                </div>
-                                                                <div class="modal-footer">
-                                                                    <button type="button" class="btn btn-secondary"
-                                                                        data-bs-dismiss="modal">Cancelar</button>
-                                                                    <a href="/excluir-acordo/{{ $acordo->id }}"><button
-                                                                            type="button"
-                                                                            class="btn btn-danger">Excluir</button></a>
-                                                                </div>
+                                                                <button type="button" class="btn-close"
+                                                                    data-bs-dismiss="modal" aria-label="Close"></button>
+                                                            </div>
+                                                            <div class="modal-body" style="color:#e24444;">
+                                                                <br />
+                                                                <p class="fw-bold alert  text-center">Você
+                                                                    realmente deseja excluir
+                                                                    <br>
+                                                                    <span class="fw-bolder fs-5">
+                                                                        {{ $acordo->nome }}</span>
+                                                                </p>
+                                                            </div>
+                                                            <div class="modal-footer ">
+                                                                <button type="button" class="btn btn-secondary"
+                                                                    data-bs-dismiss="modal">Cancelar
+                                                                </button>
+                                                                <a href="/excluir-acordo/{{ $acordo->id }}">
+                                                                    <button type="button" class="btn btn-danger">Excluir
+                                                                        permanentemente
+                                                                    </button>
+                                                                </a>
                                                             </div>
                                                         </div>
                                                     </div>
+                                                </div>
+                                        </center>
 
-
-
-                                                    <!--Fim Modal-->
-                                                    <a href="/editar-acordo/{{ $acordo->id }}"><button type="submit"
-                                                            class="btn btn-outline-warning btn-sm"><i
-                                                                class="bi bi-pencil"></i></button></a>
-                                                </td>
-                                        </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </fieldset>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                        </table>
+                    </div>
                 </div>
+
             </div>
         </div>
-    </div>
-@endsection
+        <script>
+            var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-tt="tooltip"]'))
+            var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+                return new bootstrap.Tooltip(tooltipTriggerEl)
+            })
+        </script>
+    @endsection
--- resources/views/acordos/incluir-acordo.blade.php
+++ resources/views/acordos/incluir-acordo.blade.php
@@ -1,79 +1,76 @@
 @extends('layouts.app')
-
-<title>Novo Acordo</title>
-
+@section('head')
+    <title>Novo Acordo</title>
+@endsection
 @section('content')
     <div class="container">
-        <div class="justify-content-center">
-            <div class="col-12">
-                <br>
-                <fieldset class="border rounded border-primary ">
-                    <div class="card">
+
+        <div class="container">
+            <div class="justify-content-center">
+                <div class="col-12">
+                    <br>
+
+                    <div class="card" style="border-color: #5C7CB6;">
                         <div class="card-header">
-                            <DIV class="ROW">
+                            <div class="ROW">
                                 <div class="col-12">
-                                    <span style="color: rgb(16, 19, 241); font-size:15px;">Novo Acordo</span>
+                                    Novo Acordo
                                 </div>
-                            </DIV>
+                            </div>
                         </div>
                         <div class="card-body">
-                            <form method="post" action="/armazenar-acordos/{{ $funcionario->id }}" enctype="multipart/form-data">
+
+                            <form method="post" action="/armazenar-acordos/{{ $funcionario->id }}"
+                                enctype="multipart/form-data">
                                 @csrf
                                 <div class="row">
                                     <div class="col-5">
-                                        <div class="card" style="padding: 0px">
-                                            <div class="card-body bg-body-secondary" value="{{$funcionario->id }}">
-                                                <span
-                                                    style="color: rgb(16, 19, 241)">{{ $funcionario->nome_completo }}</span>
-                                            </div>
-                                        </div>
+                                        <input class="form-control" type="text" value="{{ $funcionario->nome_completo }}"
+                                            id="iddt_inicio" name="dt_inicio" required="required" disabled>
                                     </div>
                                 </div>
                                 <hr>
-                                <div class="form-group row">
-                                    <div class="form-group col-2">Tipo de Acordo
-                                        <select class="form-select"  name="tipo_acordo" required="required">
+                                <div class="form-group row mt-2">
+                                    <div class="form-group col-md-6 col-lg-3 mt-lg-0 mt-md-0 mt-2">Tipo de Acordo
+                                        <select class="form-select" name="tipo_acordo" required="required">
                                             @foreach ($tipoacordo as $tiposacordos)
                                                 <option value="{{ $tiposacordos->id }}">{{ $tiposacordos->nome }}</option>
                                             @endforeach
                                         </select>
                                     </div>
 
-                                    <div class="form-group col-3">Data de Inicio
+                                    <div class="form-group col-md-6 col-lg-3 mt-lg-0 mt-md-0 mt-3">Data de Inicio
                                         <input class="form-control" type="date" value="" id="iddt_inicio"
                                             name="dt_inicio" required="required">
                                     </div>
-                                    <div class="form-group col-3">Data de Fim
+                                    <div class="form-group col-md-6 col-lg-3 mt-lg-0  mt-3">Data de Fim
                                         <input class="form-control" type="date" value="" id="iddt_fim"
                                             name="dt_fim">
                                     </div>
-                                    <div class="form-group col-3">Arquivo
-                                        <input type="file" class="form-control form-control-sm" name ="ficheiro" id="idficheiro" required="required" >
+                                    <div class="form-group col-md-6 col-lg-3 mt-lg-0  mt-3">Arquivo
+                                        <input type="file" class="form-control form-control-sm mb-2" name="ficheiro"
+                                            id="idficheiro" required="required" >
                                     </div>
                                 </div>
                                 <br>
                                 <div class="row">
-                                    <div class="mb-3">
+                                    <div class="mb-3 mt-md-0 mt-3">
                                         <label for="exampleFormControlTextarea1" class="form-label">Observação</label>
-                                        <textarea class="form-control " id="idobservacao" rows="1" name="observacao"></textarea>
+                                        <textarea class="form-control " id="idobservacao" rows="1" name="observacao" style="height:100px"></textarea>
                                     </div>
                                 </div>
-                                <br>
-                                <div class="row">
-                                    <div class="d-grid gap-1 col-2 mx-auto">
-                                        <a class="btn btn-danger btn-sm" href="/gerenciar-acordos/{{ $funcionario->id }}"
-                                            role="button">Cancelar</a>
-                                    </div>
-                                    <div class="d-grid gap-2 col-2 mx-auto">
-                                        <button type="submit" class="btn btn-primary btn-sm"
-                                            id="sucesso">Confirmar</button>
-                                    </div>
-                                </div>
+                                <center>
+                                    <a class="btn btn-danger col-md-3 col-5 mt-3"
+                                        href="/gerenciar-acordos/{{ $funcionario->id }}" role="button">Cancelar</a>
+                                    <button type="submit" class="btn btn-primary col-md-3 col-5 mt-5 offset-md-5 offset-1 "
+                                        id="sucesso">Confirmar
+                                    </button>
+                                </center>
                             </form>
                         </div>
                     </div>
+                </div>
             </div>
         </div>
-    </div>
     </div>
 @endsection
