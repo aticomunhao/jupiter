8fd0c7e483244a0835f040b399e4d497c1c5c2f3
MathiasColetti <mathiasc2004@gmail.com>
2024-03-26 15:09:03 -0300

    minimo 15 dias

--- app/Http/Controllers/GerenciarFeriasController.php
+++ app/Http/Controllers/GerenciarFeriasController.php
@@ -291,11 +291,10 @@ class GerenciarFeriasController extends Controller
                     $data_inicio_terceiro_periodo->diffInDays($data_fim_terceiro_periodo) + 1 . " no terceiro periodo");
             } //Verifica se o funcionario utililizou dias a menos
             elseif ($dias_de_ferias_utilizadas < $diasDeDireitoDoFuncionario) {
-
                 app('flasher')->addError('Ainda não utilizou todos os dias de ferias. <br> Utilizou:' . $data_inicio_primeiro_periodo->diffInDays($data_fim_primeiro_periodo) + 1 .
                     'no primeiro periodo.<br> E ' . $data_inicio_segundo_periodo->diffInDays($data_fim_segundo_periodo) + 1 . ' no segundo . <br>' .
                     $data_inicio_terceiro_periodo->diffInDays($data_fim_terceiro_periodo) + 1 . " no terceiro periodo");
-            } else {
+            } elseif (($data_inicio_primeiro_periodo->diffInDays($data_fim_primeiro_periodo) + 1 > 15) || ($data_inicio_segundo_periodo->diffInDays($data_fim_segundo_periodo) + 1 > 15) || ($data_inicio_terceiro_periodo->diffInDays($data_fim_terceiro_periodo) + 1) > 15) {
                 DB::table('ferias')->where('id', $ferias->id)->update([
                     'dt_ini_a' => $data_inicio_primeiro_periodo,
                     'dt_fim_a' => $data_fim_primeiro_periodo,
@@ -311,6 +310,8 @@ class GerenciarFeriasController extends Controller
                     'venda_um_terco' => (int)$request->input('periodoDeVendaDeFerias'),
                 ]);
                 app('flasher')->addCreated($funcionario->nome_completo . ' teve férias adicionadas com sucesso.');
+            } else {
+                app('flasher')->addError('É essencial que pelo menos um periodos tenha o minimo de 14 dias.');
             }
         }
         return redirect()->route('IndexGerenciarFerias');
@@ -484,8 +485,7 @@ class GerenciarFeriasController extends Controller
             ->update([
                 'motivo_retorno' => $request->input('motivo_da_recusa'),
                 'status_pedido_ferias' => 4
-            ])
-            ;
+            ]);
 
         return redirect()->route('AdministrarFerias');
     }
