ddcab6975eee3cc34885c5e9a7df9d084c2e6d96
MathiasColetti <mathiasc2004@gmail.com>
2024-04-09 16:42:39 -0300

    Logout Menu

--- resources/views/layouts/menuprincipal.blade.php
+++ resources/views/layouts/menuprincipal.blade.php
@@ -3,11 +3,12 @@
         <div class="container">
             <a class="navbar-brand" style="font-weight:bold; font-size: 28px; color:#ffffff;" href="../">Júpiter</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkdropdown"
-                aria-controls="navbarNavDarkdropdown" aria-expanded="false" aria-label="Toggle navigation" style="border:none; color:#3d5b9c;">
+                aria-controls="navbarNavDarkdropdown" aria-expanded="false" aria-label="Toggle navigation"
+                style="border:none; color:#3d5b9c;">
                 <span class="navbar-toggler-icon" style="color:"></span>
             </button>
             <div class=" navbar-collapse" id="navbarNavDarkdropdown">
-                <ul class="navbar-nav" >
+                <ul class="navbar-nav">
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="1" role="button"
                             data-bs-toggle="dropdown" aria-expanded="false" style="color:#ffffff;">Área de Pessoal</a>
@@ -24,15 +25,15 @@
                             data-bs-toggle="dropdown" aria-expanded="false" style="color:#ffffff;">Gerenciar</a>
                         <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarDarkdropdownMenuLink">
 
-                            <li><a class="dropdown-item" href="/gerenciar-entidades-de-ensino">Gerenciar Entidades de Ensino</a>
+                            <li><a class="dropdown-item" href="/gerenciar-entidades-de-ensino">Gerenciar Entidades de
+                                    Ensino</a>
                             <li><a class="dropdown-item" href="/gerenciar-setor">Gerenciar Setores</a></li>
                             <li><a class="dropdown-item" href="/gerenciar-hierarquia">Gerenciar Hierarquia</a></li>
                             <!--<li><a href="/gerenciar-cargos-regulares" class="dropdown-item">Gerenciar Cargos Regulares</a></li>
                        // <li><a href="/gerenciar-funcao-gratificada"class="dropdown-item">Gerenciar Funcao Gratificada</a></li> -->
                             <li><a href="/gerenciar-tipo-desconto" class="dropdown-item">Gerenciar Tipo de Desconto</a>
                             </li>
-                            <li><a href="/gerenciar-usuario" class="dropdown-item">Gerenciar Usuários</a>
-                            </li>
+
                             <li><a href="{{ route('gerenciar.cargos') }}" class="dropdown-item">Gerenciar Cargos</a>
                             </li>
 
@@ -45,14 +46,15 @@
                             data-bs-toggle="dropdown" aria-expanded="false" style="color:#ffffff;">Direitos
                             Remuneratórios</a>
                         <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarDarkdropdownMenuLink">
-                            <li><a href="{{ route('IndexGerenciarFerias') }}" class="dropdown-item">Periodo de Ferias</a>
+                            <li><a href="{{ route('IndexGerenciarFerias') }}" class="dropdown-item">Periodo de
+                                    Ferias</a>
                             </li>
                             <li><a href="{{ route('AdministrarFerias') }}" class="dropdown-item">Gerenciar Ferias</a>
                             </li>
                         </ul>
                     </li>
                 </ul>
-                <ul class="navbar-nav" >
+                <ul class="navbar-nav">
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="1" role="button"
                             data-bs-toggle="dropdown" aria-expanded="false" style="color:#ffffff;">Relatórios</a>
@@ -62,41 +64,31 @@
                         </ul>
                     </li>
                 </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="1" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false" style="color:#ffffff;">Administrar
+                            Sistema</a>
+                        <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarDarkdropdownMenuLink">
+                            <li><a href="/gerenciar-usuario" class="dropdown-item">Gerenciar Usuários</a>
+                            </li>
+
+                        </ul>
+                    </li>
+                </ul>
 
                 <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ms-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        @if (Route::has('login'))
-                            <li class="nav-item">
-                                <a class="nav-link" style="color:#ffffff;"
-                                    href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                        @endif
-                        {{--
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" style="color:#ffffff;" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif --}}
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbardropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbardropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="4" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: #ffffff">Logout</a>
+                        <ul class="dropdown-menu " aria-labelledby="navbarDarkDropdownMenuLink">
+                            <li><a class="dropdown-item" href="/usuario/alterar-senha"><i class="mdi mdi-lock-open-outline font-size-17 text-muted align-middle mr-1"></i>Alterar Senha</a></li>
+                            <li><a class="dropdown-item" href="javascript:void();" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="mdi mdi-power font-size-17 text-muted align-middle mr-1 text-danger"></i> {{ __('Sair') }}</a></li>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </ul>
+                    </li>
                 </ul>
                 </li>
                 </ul>
