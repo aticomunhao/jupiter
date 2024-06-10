225dac7393b78630eaf8cf082f61ff9e6c7a250a
MathiasColetti <mathiasc2004@gmail.com>
2024-06-10 16:04:16 -0300

    Visualizar Seto

--- resources/views/setores/gerenciar-setor.blade.php
+++ resources/views/setores/gerenciar-setor.blade.php
@@ -16,31 +16,31 @@
                     <div>
                         <div class="card-body">
                             <form class="justify-content-center" action="{{ route('pesquisar-setor') }}"
-                                class="form-horizontal" method="GET">
+                                  class="form-horizontal" method="GET">
                                 <div class="row mt-2">
                                     <div class="col-3">Setor
                                         <input class="form-control" style="border: 1px solid #999999; padding: 5px;"
-                                            maxlength="50" type="text" id="2" name="nome"
-                                            value="{{ $nome }}">
+                                               maxlength="50" type="text" id="2" name="nome"
+                                               value="{{ $nome }}">
                                     </div>
                                     <div class="col-1">Sigla
                                         <input class="form-control" style="border: 1px solid #999999; padding: 5px;"
-                                            maxlength="30" type="text" id="3" name="sigla"
-                                            value="{{ $sigla }}">
+                                               maxlength="30" type="text" id="3" name="sigla"
+                                               value="{{ $sigla }}">
                                     </div>
                                     <div class="col-2">Data de Início
                                         <input class="form-control" style="border: 1px solid #999999; padding: 5px;"
-                                            maxlength="30" type="date" id="3" name="dt_inicio"
-                                            value="{{ $dt_inicio }}">
+                                               maxlength="30" type="date" id="3" name="dt_inicio"
+                                               value="{{ $dt_inicio }}">
                                     </div>
                                     <div class="col-2">Data de Fim
                                         <input class="form-control" style="border: 1px solid #999999; padding: 5px;"
-                                            maxlength="30" type="date" id="3" name="dt_fim"
-                                            value="{{ $dt_fim }}">
+                                               maxlength="30" type="date" id="3" name="dt_fim"
+                                               value="{{ $dt_fim }}">
                                     </div>
                                     <div class="col-1">Status
                                         <select class="form-select" style="border: 1px solid #999999; padding: 5px;"
-                                            id="4" name="status" type="number">
+                                                id="4" name="status" type="number">
                                             <option value="">Todos</option>
                                             <option value="1">Ativo</option>
                                             <option value="2">Inativo</option>
@@ -48,17 +48,21 @@
                                     </div>
                                     <div class="col" style="margin-top:22px">
                                         <input class="btn btn-light btn-sm"
-                                            style="box-shadow: 1px 2px 5px #000000; font-size: 1rem"
-                                            type="submit" value="Pesquisar">
+                                               style="box-shadow: 1px 2px 5px #000000; font-size: 1rem"
+                                               type="submit" value="Pesquisar">
 
-                                        <a href="/gerenciar-setor"><button class="btn btn-light btn-sm" type="button"
-                                                value=""
-                                                style="box-shadow: 1px 2px 5px #000000; margin:2px; font-size: 1rem">Limpar</button></a>
+                                        <a href="/gerenciar-setor">
+                                            <button class="btn btn-light btn-sm" type="button"
+                                                    value=""
+                                                    style="box-shadow: 1px 2px 5px #000000; margin:2px; font-size: 1rem">
+                                                Limpar
+                                            </button>
+                                        </a>
                             </form>
 
                             <a href="/incluir-setor"><input class="btn btn-success btn-sm" type="button" name="6"
-                                    value="Novo Cadastro +"
-                                    style="box-shadow: 1px 2px 5px #000000; font-size: 1rem"></a>
+                                                            value="Novo Cadastro +"
+                                                            style="box-shadow: 1px 2px 5px #000000; font-size: 1rem"></a>
 
                         </div>
                     </div>
@@ -68,89 +72,111 @@
                         <table
                             class="table table-sm table-striped table-bordered border-secondary table-hover align-middle">
                             <thead style="text-align: center;">
-                                <tr style="background-color: #d6e3ff; font-size:17px; color:#000000">
-                                    <th class="col-4">Setor</th>
-                                    <th class="col-1">Sigla</th>
-                                    <th class="col">Data Inicio</th>
-                                    <th class="col">Data Final</th>
-                                    <th class="col-1">Status</th>
-                                    <th class="col">Substituto</th>
-                                    <th class="col">Ações</th>
+                            <tr style="background-color: #d6e3ff; font-size:17px; color:#000000">
+                                <th class="col-4">Setor</th>
+                                <th class="col-1">Sigla</th>
+                                <th class="col">Data Inicio</th>
+                                <th class="col">Data Final</th>
+                                <th class="col-1">Status</th>
+                                <th class="col">Substituto</th>
+                                <th class="col">Ações</th>
 
-                                </tr>
+                            </tr>
                             </thead>
                             <tbody style="font-size: 16px; color:#000000; text-align: center">
+                            @foreach ($lista_setor as $lista_setores)
                                 <tr>
-                                    @foreach ($lista_setor as $lista_setores)
-                                        <td scope="">
-                                            {{ $lista_setores->nome }}
-                                        </td>
-                                        <td scope="">
-                                            {{ $lista_setores->sigla }}
-                                        </td>
-                                        <td scope="">
-                                            {{ Carbon\Carbon::parse($lista_setores->dt_inicio)->format('d-m-Y') }}
-                                        </td>
-                                        <td scope="">
-                                            @if (!empty($lista_setores->dt_fim))
-                                                {{ Carbon\Carbon::parse($lista_setores->dt_fim)->format('d-m-Y') }}
-                                            @endif
-                                        </td>
-                                        <td scope="">
-                                            {{ $lista_setores->status }}
-                                        </td>
-                                        <td scope="">
-                                            {{ $lista_setores->nome_substituto }}
-                                        </td>
+
+                                    <td scope="">
+                                        {{ $lista_setores->nome }}
+                                    </td>
+                                    <td scope="">
+                                        {{ $lista_setores->sigla }}
+                                    </td>
+                                    <td scope="">
+                                        {{ Carbon\Carbon::parse($lista_setores->dt_inicio)->format('d-m-Y') }}
+                                    </td>
+                                    <td scope="">
+                                        @if (!empty($lista_setores->dt_fim))
+                                            {{ Carbon\Carbon::parse($lista_setores->dt_fim)->format('d-m-Y') }}
+                                        @endif
+                                    </td>
+                                    <td scope="">
+                                        {{ $lista_setores->status }}
+                                    </td>
+                                    <td scope="">
+                                        {{ $lista_setores->nome_substituto }}
+                                    </td>
 
 
-                                        <td scope="" style="text-align: center">
+                                    <td scope="" style="text-align: center">
 
 
-                                            <a href="/editar-setor/{{ $lista_setores->ids }}"><button type="button"
-                                                    class="btn btn-outline-warning"><i class="bi-pencil"
-                                                        style="font-size: 1rem; color:#303030;"></i></button></a>
-                                            <a href="/carregar-dados/{{ $lista_setores->ids }}"><button type="button"
-                                                    class="btn btn-outline-primary"><i class="bi bi-arrow-left-right"
-                                                        style="font-size: 1rem;color:#303030; "></i></button></a>
-                                            <a href=""><button type="button" class="btn btn-outline-primary"><i
-                                                        class="bi-search"
-                                                        style="font-size: 1rem; color:#303030;"></i></button></a>
-                                            <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal"
-                                                data-bs-target="#A{{ $lista_setores->ids }}"
-                                                class="btn btn-outline-danger"><i class="bi-trash"
-                                                    style="font-size: 1rem; color:#303030;"></i></button>
+                                        <a href="/editar-setor/{{ $lista_setores->ids }}"
+                                           class="btn btn-outline-warning">
 
-                                            <!-- Modal -->
-                                            <div class="modal fade" id="A{{ $lista_setores->ids }}" tabindex="-1"
-                                                aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                <div class="modal-dialog">
-                                                    <div class="modal-content">
-                                                        <div class="modal-header" style="background-color:#DC4C64;">
-                                                            <h5 class="modal-title" id="exampleModalLabel"
-                                                                style=" color:rgb(255, 255, 255)">Excluir Setor</h5>
-                                                            <button type="button" class="btn-close"
+                                            <i class="bi-pencil"
+                                               style="font-size: 1rem; color:#303030;"></i>
+
+                                        </a>
+                                        <a href="/carregar-dados/{{ $lista_setores->ids }}"
+                                           class="btn btn-outline-primary">
+
+                                            <i class="bi bi-arrow-left-right"
+                                               style="font-size: 1rem;color:#303030; "></i>
+
+                                        </a>
+
+                                        <a href="{{route('show.setor', ['ids' => $lista_setores->ids])}}" type="button"
+                                           class="btn btn-outline-primary"><i
+                                                class="bi-search"
+                                                style="font-size: 1rem; color:#303030;"></i>
+                                        </a>
+
+                                        <a type="button" class="btn btn-outline-danger" data-bs-toggle="modal"
+                                           data-bs-target="#A{{ $lista_setores->ids }}"
+                                           class="btn btn-outline-danger"><i class="bi-trash"
+                                                                             style="font-size: 1rem; color:#303030;"></i>
+                                        </a>
+
+                                        <!-- Modal -->
+                                        <div class="modal fade" id="A{{ $lista_setores->ids }}" tabindex="-1"
+                                             aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                    <div class="modal-header" style="background-color:#DC4C64;">
+                                                        <h5 class="modal-title" id="exampleModalLabel"
+                                                            style=" color:rgb(255, 255, 255)">Excluir Setor</h5>
+                                                        <button type="button" class="btn-close"
                                                                 data-bs-dismiss="modal" aria-label="Close"></button>
-                                                        </div>
-                                                        <div class="modal-body">
-                                                            Você realmente deseja excluir o setor
-                                                            <br>
-                                                            <span style="color:#DC4C64; font-weight: bold">
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        Você realmente deseja excluir o setor
+                                                        <br>
+                                                        <span style="color:#DC4C64; font-weight: bold">
                                                                 {{ $lista_setores->nome }}</span> ?
-                                                        </div>
-                                                        <div class="modal-footer mt-2">
-                                                            <button type="button" class="btn btn-danger"
-                                                                data-bs-dismiss="modal">Cancelar</button>
-                                                            <a type="button" class="btn btn-primary"
-                                                                href="/excluir-setor/{{ $lista_setores->ids }}">Confirmar
-                                                            </a>
-                                                        </div>
+                                                    </div>
+                                                    <div class="modal-footer mt-2">
+                                                        <button type="button" class="btn btn-danger"
+                                                                data-bs-dismiss="modal">Cancelar
+                                                        </button>
+                                                        <a type="button" class="btn btn-primary"
+                                                           href="/excluir-setor/{{ $lista_setores->ids }}">Confirmar
+                                                        </a>
                                                     </div>
                                                 </div>
                                             </div>
-                                            <!--Fim Modal-->
-                                        </td>
+                                        </div>
+                                        <!--Fim Modal-->
+                                    </td>
                                 </tr>
-                                </tr>
-                                @endforeach
-                            @endsection
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
