2d089e361874246744e8ef4ed3f293bd32da4a2c
Moises <moisesshalon72@gmail.com>
2024-07-29 17:15:44 -0300

    corrige script uf/cidade

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,49 @@
 import './bootstrap';
 
-import './custom';
+import $ from 'jquery';
 
 import 'select2';
+
+import './custom';
+
+window.$ = window.jQuery = $;
+
+$(document).ready(function () {
+    $('#cidade1, #cidade2, #setorid').select2({
+        theme: 'bootstrap-5',
+        width: '100%',
+    });
+
+    function populateCities(selectElement, stateValue) {
+        $.ajax({
+            type: "get",
+            url: "/retorna-cidade-dados-residenciais/" + stateValue,
+            dataType: "json",
+            success: function (response) {
+                selectElement.empty();
+                $.each(response, function (indexInArray, item) {
+                    selectElement.append('<option value="' + item.id_cidade + '">' + item.descricao + '</option>');
+                });
+            },
+            error: function (xhr, status, error) {
+                console.error("An error occurred:", error);
+            }
+        });
+    }
+
+    $('#uf1').change(function (e) {
+        var stateValue = $(this).val();
+        $('#cidade1').removeAttr('disabled');
+        populateCities($('#cidade1'), stateValue);
+    });
+
+    $('#uf2').change(function (e) {
+        var stateValue = $(this).val();
+        $('#cidade2').removeAttr('disabled');
+        populateCities($('#cidade2'), stateValue);
+    });
+
+    $('#idlimpar').click(function (e) {
+        $('#idnome_completo').val("");
+    });
+});
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -9,4 +10,10 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            $: 'jquery',
+            jQuery: 'jquery',
+        },
+    },
 });
