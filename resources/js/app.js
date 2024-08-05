edeea17ac1f7d1cea33ce6622ec17c6aba75e297
Moises <moisesshalon72@gmail.com>
2024-08-05 13:50:18 -0300

    corg erro ecmas app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ import './bootstrap';
 
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import 'bootstrap5-toggle/js/bootstrap5-toggle.ecmas';
+import 'bootstrap5-toggle/js/bootstrap5-toggle.ecmas.min.js';
 
 import $ from 'jquery';
 
