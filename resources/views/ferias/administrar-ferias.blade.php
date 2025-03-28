e7c7423848d26da36835e6a74cf998cfa4edc123
Mathias Coletti <88839208+MathiasColetti@users.noreply.github.com>
2025-03-28 10:41:14 -0300

    danger para notificacao

--- resources/views/ferias/administrar-ferias.blade.php
+++ resources/views/ferias/administrar-ferias.blade.php
@@ -371,7 +371,7 @@
 
             if (numeroDePeriodosParaAprovar > 0) {
                 $('#notificacao').append(
-                    `<div class="alert alert-warning alert-dismissible fade show" role="alert">
+                    `<div class="alert alert-danger alert-dismissible fade show" role="alert">
                 <strong>Atenção!</strong> Existem ${numeroDePeriodosParaAprovar} períodos de férias para aprovação.
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>`
